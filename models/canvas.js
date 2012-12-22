a806f65cc63e334e7760541b6dedd5291e542079
Piotr <piotder@gmail.com>
2012-12-22 13:12:39 +0100

    development commit 1

--- models/canvas.js
+++ models/canvas.js
@@ -4,17 +4,40 @@ var mongoose = require('mongoose'),
 var Canvas = new Schema({
   id : {type: Number, required: true, trim: true, unique: true}, 
   name : {type: String, required: true, trim: true},
-  width : {type: Number, required: true},
-  height : {type: Number, required: true},
-  path: String,
+  width : {type: Number, required: true, default: 800},
+  height : {type: Number, required: true, default: 600},
+  history : { 
+    arr: { type: Array, required: true, default: [] },
+    pos: { type: Number, required: true, default: 0 } 
+  },
+  path: String
 });
 
 Canvas.statics.max = function () {
   return 1000000;
-}
+};
 
 Canvas.statics.picName = function () {
   return 'pic.png';
+};
+
+Canvas.methods.push = function (commands) {
+  var length = commands.length;
+  for(var i = this.history.pos, j = 0; j === length; ) {
+    this.history.arr[i++] = commands[j++];
+  }
+};
+
+Canvas.methods.redo = function () {
+  if(this.history.pos < this.history.arr.length) {
+    ++pos;
+  }
+};
+
+Canvas.methods.undo = function () {
+  if(this.history.pos > 0) {
+    --pos;
+  }
 }
  
 module.exports = mongoose.model('Canvas', Canvas);
